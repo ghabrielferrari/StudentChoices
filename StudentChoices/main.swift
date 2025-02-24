d1478f437edc1717fe3e626ed3d7442b33ca617b
ghabrielferrarii <ghcferrari75@gmail.com>
2025-02-24 12:23:23 -0300

    Test

--- StudentChoices/main.swift
+++ StudentChoices/main.swift
@@ -7,4 +7,39 @@
 
 import Foundation
 
-print("Hello, World!")
+
+print("Escolha uma opcao: (1- iniciar, 2- sair)")
+
+while true {
+    
+    let escolha = readLine()
+        
+        if escolha == "2"{
+            print("Jogo Encerrado")
+            break
+        }
+    
+    print("Iniciando...")
+    print("Bem-vindo ao Student Choices, preencha os dados de seu personagem: ")
+
+    print("Nome: ")
+    if let nome = readLine() {
+        print("Gênero: ")
+        if let genero = readLine() {
+            print("Faculdade: ")
+            if let faculdade = readLine() {
+                print("Curso: ")
+                if let curso = readLine() {
+                    
+                    print("\n📜 Dados do personagem 📜")
+                    print("Nome: \(nome)")
+                    print("Gênero: \(genero)")
+                    print("Faculdade: \(faculdade)")
+                    print("Curso: \(curso)")
+                }
+            }
+        }
+    }
+
+    
+}
